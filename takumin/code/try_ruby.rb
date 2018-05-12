8d07de49ea7a6fd9c11c7a62f029dd5132104db6
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2018-05-12 13:54:47 +0900

    学び

--- takumin/code/try_ruby.rb
+++ takumin/code/try_ruby.rb
@@ -1,79 +1,25 @@
-# module Watanabe
-#   module Takumi
-#     module Mirai
-#       def call_name
-#         p self
-#         puts "なんですか"
-#       end
-#     end
-#   end
-# end
-
-
-# class Karen::OI
-#   include Watanabe::Takumi::Mirai
-
-#   def call_name
-#     p self
-#     super
-#   end
-# end
-
-# p karen = Karen::OI.new.call_name
-
-# class Cat
-#   @siro = "kuro"
-#   attr_reader :siro
-
-#   def initialize(siro="siro")
-#     @siro = siro
-#   end
-
-#   def self.kuro
-#     @siro
-#   end
-# end
-
-# Cat.class_eval do
-#   def call_name
-#     p "なんだと"
-#   end
-# end
-
-
-# cat = Cat.new.call_name
-
-class Boss
-  private
-  def call_boss_private
-    p "bossssss"
+class Animal
+  def initialize(args = {})
+    @name = args[:name]
+    @size = args[:size] || defo_size
   end
 end
 
-class Takumi < Boss
-  def call_protected
-    p protected_wata
+class Dog < Animal
+  def initialize(args)
+    super(args)
   end
 
-  def call_private
-    call_boss_private
+  def defo_size
+    30
   end
-
-  def protected_wata
-    add = {karen: "いもうと"}
-    private_wata.merge(add)
-  end
-  protected :protected_wata
-
-  def private_wata
-    fami = {taku: "ani", miai: "otoutp"}
-  end
-  private :private_wata
 end
 
+# class Cat < Animal
+#   def initialize(args = {})
 
+#   end
+# end
 
-Takumi.new.call_protected
-Takumi.new.call_private
-
-
+dog = Dog.new(name: "ken")
+p dog
